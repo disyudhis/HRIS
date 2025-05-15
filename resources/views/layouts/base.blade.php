d45aaaac5bae85d1907125e49e0dfc57015e1aaf
disyudhis <yudhistiraaaa20@gmail.com>
2025-05-15 20:53:03 +0700

    change layout

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,13 +14,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Styles -->
-    @vite(['resources/css/app.css'])
     @stack('styles')
-
-    <!-- Alpine.js -->
-    @vite(['resources/js/app.js'])
+    @if (config('app.env') === 'local')
+        @vite(['resources/css/app.css'])
+        @vite(['resources/js/app.js'])
+    @endif
     @livewireStyles
 </head>
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,12 +12,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Styles -->
-    @vite(['resources/css/app.css'])
+    @if (config('app.env') === 'local')
+        @vite(['resources/css/app.css'])
+        @vite(['resources/js/app.js'])
+    @endif
 
-    <!-- Alpine.js -->
-    @vite(['resources/js/app.js'])
     @livewireStyles
 </head>
 
